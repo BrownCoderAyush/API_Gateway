6c17966763bb24f8f508b0f8f1623412072bca5f
Ayush Chopra <ayushchopra0@gmail.com>
2023-04-21 22:37:31 +0530

    added README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to API_Gateway
+
+# Project Setup 
+
+- clone the project on your local.
+- Execute `npm install` on the same path as of your root directory of the downloaded project. 
+- Create a `.env` file in the root directory and add the following environment variable.
+
+```
+AUTH_SERVICE_PATH='http://localhost:3001'
+```
+
+### This service acts as a Gateway to all other `microservices` under `AIRLINE_MANAGEMENT_SYSTEM` 
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-// const axios = require('axios');
+const axios = require('axios');
 const rateLimit = require('express-rate-limit');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const { isAuthenticated } = require('./authentication_middleware');
@@ -11,22 +11,21 @@ const PORT = 3005;
 
 const limiter = rateLimit({
 	windowMs: 2 * 60 * 1000, // 15 minutes
-	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	
 })
 
+// log the request details 
 app.use(morgan('combined'));
+
+
 app.use(limiter);
 
 app.use('/bookingservice' , isAuthenticated);
 
+
 app.use('/bookingservice' , createProxyMiddleware({ target : 'http://localhost:3002/' , changeOrigin:true}));
 
-app.get('/home',(req,res)=>{
-    res.json({
-        status:'ok'
-    })
-})
 
 app.listen(PORT , async()=>{
     console.log(`Server started at port ${PORT}`);
